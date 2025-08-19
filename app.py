ca71cefe75907f3614b02563c89799753866d376
KaramiZahra <karamizahra27k@gmail.com>
2025-08-19 18:45:11 +0330

    feat: implement add_transaction

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import datetime
+
 transactions = []
 
 
@@ -6,7 +9,51 @@ def save_transactions():
 
 
 def add_transaction():
-    pass
+
+    while True:
+        transaction_type = input(
+            "Enter transaction type 1)Income 2)Expense: ").strip()
+        if transaction_type == "1":
+            transaction_type = "Income"
+            break
+        elif transaction_type == "2":
+            transaction_type = "Expense"
+            break
+        else:
+            print("Invalid type.")
+
+    transaction_category = input(
+        "Enter transaction category: ").strip().capitalize()
+
+    while True:
+        try:
+            transaction_amount = float(
+                input("Enter transaction amount: ").strip())
+            break
+        except ValueError:
+            print("Invalid amount. Enter a number.")
+
+    while True:
+        date_input = input("Enter transaction date(YYYY-MM-DD): ").strip()
+        try:
+            transaction_date = datetime.strptime(date_input, "%Y-%m-%d").date()
+            break
+        except ValueError:
+            print("Invalid date. Use YYYY-MM-DD format.")
+
+    transaction_note = input("Enter transaction note: ").strip()
+
+    new_transaction = {
+        'id': str(uuid.uuid4()),
+        'type': transaction_type,
+        'category': transaction_category,
+        'amount': transaction_amount,
+        'date': transaction_date,
+        'note': transaction_note
+    }
+    transactions.append(new_transaction)
+
+    print(transactions)
 
 
 def menu():
